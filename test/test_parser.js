16c84f1ab65a75bd0ddf08a587e718adf949a1ab
David Wolever <david@wolever.net>
2011-12-23 20:26:20 -0500

    Removing un-used test_parser.js

--- test/test_parser.js
+++
@@ -1,9 +0,0 @@
-require(["test/common", "remora/parser", "underscore"],
-function(common, parser, _) {
-
-  common.runTests(testCases, function(testcase) {
-    var actual = parser.parse(testcase.input);
-    deepEqual(actual, testcase.expected_ast);
-  });
-
-});
