918ccf234c1a6c994efaaca6e431884b22b1ed84
David Wolever <david@wolever.net>
2012-01-05 02:51:29 -0500

    Move files which only changed capitlization back into place
    --HG--
    rename : src/remora/AST2JS.x.js => src/remora/AST2JS.js
    rename : src/remora/ASTTransforms.x.js => src/remora/ASTTransforms.js
    rename : src/remora/ASTWalker.x.js => src/remora/ASTWalker.js

--- src/browser/deps.js
+++ src/browser/deps.js
@@ -2,9 +2,9 @@
 // Please do not edit.
 goog.addDependency('../browser/base.js', ['goog'], []);
 goog.addDependency('../remora.js', ['remora'], ['remora.AST2JS', 'remora.ASTTransforms', 'remora.evaler', 'remora.parser', 'remora.utils']);
-goog.addDependency('../remora/AST2JS.x.js', ['remora.AST2JS'], ['remora.ASTWalker']);
-goog.addDependency('../remora/ASTTransforms.x.js', ['remora.ASTTransforms'], ['remora.ASTWalker']);
-goog.addDependency('../remora/ASTWalker.x.js', ['remora.ASTWalker'], []);
+goog.addDependency('../remora/AST2JS.js', ['remora.AST2JS'], ['remora.ASTWalker']);
+goog.addDependency('../remora/ASTTransforms.js', ['remora.ASTTransforms'], ['remora.ASTWalker']);
+goog.addDependency('../remora/ASTWalker.js', ['remora.ASTWalker'], []);
 goog.addDependency('../remora/evaler.js', ['remora.evaler'], []);
 goog.addDependency('../remora/parser.js', ['remora.parser'], []);
 goog.addDependency('../remora/utils.js', ['remora.utils'], []);
